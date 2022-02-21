771ff20da2e24cb81814187356e40f87540c98f2
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2022-02-22 01:31:55 +0900

    Create 2954.py

---
+++ 2954.py
@@ -0,0 +1,9 @@
+str1 = input()
+
+str1 = str1.replace('apa', 'a')
+str1 = str1.replace('epe', 'e')
+str1 = str1.replace('ipi', 'i')
+str1 = str1.replace('opo', 'o')
+str1 = str1.replace('upu', 'u')
+
+print(str1)
