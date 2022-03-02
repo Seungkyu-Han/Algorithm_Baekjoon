c9f593905b084542fc8726ca682703c356f9f796
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2022-03-03 00:42:26 +0900

    Create 17362.py

---
+++ 17362.py
@@ -0,0 +1,9 @@
+num = int(input())
+
+res = num % 8
+
+if res == 1: print(1)
+elif res == 2 or res == 0: print(2)
+elif res == 3 or res == 7: print(3)
+elif res == 4 or res == 6: print(4)
+else: print(5)
