9b686df1943d465367a8c3742d49310635dd3dfd
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2022-05-17 02:00:36 +0900

    Create 24480.py

---
+++ 24480.py
@@ -0,0 +1,32 @@
+import sys
+
+N, M, R = map(int, sys.stdin.readline().split())
+
+graph = dict()
+
+for i in range(N):
+    graph[i+1] = []
+
+for i in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(N):
+    graph[i+1] = sorted(graph[i+1])
+
+need_visit = [R]
+visited = [True for i in range(N)]
+result = [0 for i in range(N)]
+cnt = 1
+while need_visit:
+    data = need_visit.pop()
+    if visited[data - 1]:
+        need_visit.extend(graph[data])
+        visited[data - 1] = False
+        result[data - 1] = cnt
+        cnt += 1
+
+
+for i in result:
+    print(i)
